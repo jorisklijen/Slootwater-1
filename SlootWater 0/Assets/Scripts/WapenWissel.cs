91a5580b97541ce7f5a697885d27807a27b8c7b4
jorisklijen <34738260+jorisklijen@users.noreply.github.com>
2019-06-24 13:29:04 +0200

    kechies gaan dood aan slootwater2

---
+++ SlootWater 0/Assets/Scripts/WapenWissel.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class WapenWissel : MonoBehaviour
+{
+    public int selectedWapon = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectedWapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int priviusSelectedWapon = selectedWapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (selectedWapon >= transform.childCount - 1)
+            {
+                selectedWapon = 0;
+            }
+            else
+            {
+                selectedWapon++;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (selectedWapon <= 0)
+            {
+                selectedWapon = transform.childCount - 1;
+            }
+            else
+            {
+                selectedWapon--;
+            }
+        }
+
+        if (priviusSelectedWapon != selectedWapon)
+        {
+            SelectedWapon();
+        }
+
+    }
+
+    void SelectedWapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == selectedWapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            i++;
+        }
+
+    }
+}
