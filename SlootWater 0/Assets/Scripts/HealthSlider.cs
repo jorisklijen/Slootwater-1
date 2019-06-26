a1a8a23237c068f43ad53ffd8dc103074af83f32
joris kleijn <34738260+jorisklijen@users.noreply.github.com>
2019-06-26 19:21:39 +0200

    slider bugg

---
+++ SlootWater 0/Assets/Scripts/HealthSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSlider : MonoBehaviour
+{ 
+    public Transform healthBar;
+    public Transform maxHelthBar;
+
+    private void Start()
+    {
+        float healthValue = gameObject.GetComponent<Health>().health;
+        float maaxHealthValue = gameObject.GetComponent<Health>().maxHealth;
+
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Health health = gameObject.GetComponent<Health>();
+        Debug.Log(health == null);
+        healthBar.GetComponent<Image>().fillAmount = health.Get();
+        maxHelthBar.GetComponent<Image>().fillAmount = health.GetMax();
+    }
+}
