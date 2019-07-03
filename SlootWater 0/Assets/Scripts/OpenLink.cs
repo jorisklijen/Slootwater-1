c66dfd8199f7e8fb3ef1791beca8702b85197d84
jorisklijen <34738260+jorisklijen@users.noreply.github.com>
2019-07-03 20:20:30 +0200

    github knop

---
+++ SlootWater 0/Assets/Scripts/OpenLink.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void GitHub()
+    {
+        Debug.Log("laad git hub");
+        Application.OpenURL("https://github.com/jorisklijen/Slootwater-1");
+    }
+}
